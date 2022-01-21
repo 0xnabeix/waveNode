c35c322ad0b42b080c3beae2c7506c776cc6a61a
torutona <sololouve@gmail.com>
2022-01-22 05:54:42 +0900

    add commit

--- contracts/Wave.sol
+++ contracts/Wave.sol
@@ -1754,11 +1754,16 @@ contract PaymentSplitter is Context {
         require(_shares[account] > 0, "PaymentSplitter: account has no shares");
 
         uint256 totalReceived = address(this).balance + totalReleased();
+
+        /**
+        * @dev The amount of payment tha users should be received.
+        */
+
         uint256 payment = _pendingPayment(
             account,
             totalReceived,
             released(account)
-        );
+        );  
 
         require(payment != 0, "PaymentSplitter: account is not due payment");
 
